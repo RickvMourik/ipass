acbc8b86d8e33794266cf2cb9bca248f47f0207c
Rick van Mourik <43366526+RickvMourik@users.noreply.github.com>
2019-07-04 09:15:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main( void ){
    bool inactX = 0;                    //1 to enable inactivity check for the axis, 0 to disable
    bool inactY = 0;
    bool inactZ = 0;
-   bool act_inact_tot[] = {act_ac_dc, actX, actY, actZ, inact_ac_dc, inactX, inactY, inactZ};
+   bool act_inact_tot[8] = {inactZ, inactY, inactX, inact_ac_dc, actZ, actY, actX, act_ac_dc};
 
    sensor.setLatent(0);                //set time window to before double tap can be measured (1.25 ms/LSB) 0 disables double tap
    sensor.setWindow(0);                //set time window where double tap can be measured (1.25 ms/LSB) 0 disables double tap
@@ -89,7 +89,7 @@ int main( void ){
    bool freefall = 0;
    bool watermark = 0;
    bool overrun = 0;
-   bool interrupts[] = {overrun, watermark, freefall, inactivity, activity, double_tap, single_tap, data_ready};
+   bool interrupts[8] = {overrun, watermark, freefall, inactivity, activity, double_tap, single_tap, data_ready};
 
    sensor.setInterrupts(interrupts);
    
